1a4780c8671ca02f265bf3e81334ea620cac3184
Amin <amin@sudofy.com>
2018-09-26 18:59:32 +0500

    add node-ec2-metadata in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Q = require('q');
+var metadata = require('node-ec2-metadata');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
