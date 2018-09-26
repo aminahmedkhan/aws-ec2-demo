5449d35ccde5d9cb7a8874fd10bfbdea58f43ece
Amin <amin@sudofy.com>
2018-09-26 18:35:23 +0500

    project init

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  Q.all([
+    metadata.getMetadataForInstance('ami-id'),
+    metadata.getMetadataForInstance('public-ipv4'),
+    metadata.getMetadataForInstance('placement/availability-zone'),
+    metadata.getMetadataForInstance('instance-id'),
+    metadata.getMetadataForInstance('mac'),
+  ])
+    .spread(function (amiID, ipv4, az, instanceId) {
+      const data = {
+        amiID,
+        ipv4,
+        az,
+        instanceId
+      };
+      res.render('index', data);
+    })
+    .fail(function (error) {
+      console.log("Error: " + error);
+    });
+});
+
+module.exports = router;
