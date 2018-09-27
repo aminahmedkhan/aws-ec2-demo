952b0a9cb76d50c6c5034fde11ac857e92c9c107
Amin <amin@sudofy.com>
2018-09-27 13:25:06 +0500

    add offline support

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,39 @@ var router = express.Router();
 var Q = require('q');
 var metadata = require('node-ec2-metadata');
 
+var isAWS = false;
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  Q.all([
-    metadata.getMetadataForInstance('ami-id'),
-    metadata.getMetadataForInstance('public-ipv4'),
-    metadata.getMetadataForInstance('placement/availability-zone'),
-    metadata.getMetadataForInstance('instance-id'),
-    metadata.getMetadataForInstance('mac'),
-  ])
-    .spread(function (amiID, ipv4, az, instanceId) {
-      const data = {
-        amiID,
-        ipv4,
-        az,
-        instanceId
-      };
-      res.render('index', data);
-    })
-    .fail(function (error) {
-      console.log("Error: " + error);
-    });
+  if (isAWS) {
+    Q.all([
+      metadata.getMetadataForInstance('ami-id'),
+      metadata.getMetadataForInstance('public-ipv4'),
+      metadata.getMetadataForInstance('placement/availability-zone'),
+      metadata.getMetadataForInstance('instance-id'),
+      metadata.getMetadataForInstance('mac'),
+    ])
+      .spread(function (amiID, ipv4, az, instanceId) {
+        const data = {
+          amiID,
+          ipv4,
+          az,
+          instanceId
+        };
+        res.render('index', data);
+      })
+      .fail(function (error) {
+        console.log("Error: " + error);
+      });
+  } else {
+    const data = {
+      amiID: 'i-04d5ced80e4dd0bdf',
+      ipv4: '13.57.31.141',
+      az: 'us-west-1b',
+      instanceId: 'ami-0782017a917e973e7'
+    };
+    res.render('index', data);
+  }
+
 });
 
 module.exports = router;
